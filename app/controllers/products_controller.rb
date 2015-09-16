c727e6666749860afdf0a26bb97e0aebf9014a2c
stivenrgalvis <jsrgalvis@gmail.com>
2015-09-16 17:13:46 -0400

    crea la ruta post y se creo el boton crear nuevo producto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,19 @@ class ProductsController <ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "El producto fue publicado con éxito"
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :url, :description)
+    end
+
 end
