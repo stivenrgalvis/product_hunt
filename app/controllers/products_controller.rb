65bd051f9a8c081d24b9eb45254fa74320fe04d2
stivenrgalvis <jsrgalvis@gmail.com>
2015-10-04 13:19:37 -0400

    crea la ruta /product/id/edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,12 +9,16 @@ class ProductsController <ApplicationController
   end
 
   def create
+
     @product = Product.new(product_params)
+
     if @product.save
       redirect_to products_path, notice: "El producto fue publicado con éxito"
     else
       render :new
+
     end
+
   end
 
   def  show
@@ -23,10 +27,18 @@ class ProductsController <ApplicationController
 
   end
 
+  def edit
+
+  @product = Product.find(params[:id])
+
+  end
+
   private
 
     def product_params
+
       params.require(:product).permit(:name, :url, :description)
+
     end
 
 end
---
+++ app/helpers/products_helper.rb
@@ -0,0 +1,8 @@
+module ProductsHelper
+
+def form_title
+  @product.new_record? ? "Publicar Producto"  : "Modificar Producto"
+
+end
+
+end
